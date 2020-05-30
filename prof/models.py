f7b70464188023091de3f7db7c3e56edb07357ed
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-05-30 15:03:08 +0300

    made a profile page where it enable one to set there profile and change profile picture and bio and other functions they can perform on the app

--- prof/models.py
+++ prof/models.py
@@ -6,3 +6,11 @@ class Profile(models.Model):
     profile_pic=models.ImageField(upload_to='articles/', blank=True)
     bio=models.CharField(max_length =100)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.bio
+
+    @classmethod
+    def get_profile(cls,user_id):
+        userd=cls.objects.filter(user=user_id)
+        print (userd)
+        return userd;
--- prof/views.py
+++ prof/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Profile,User
 # Create your views here..
 
 # @login_required(login_url='/accounts/login/')
 def index(request):
+    user=request.user
+    # print(user.username)
     
-    return render(request,'index.html')
+    return render(request,'index.html',{"user":user})
+
+def profile(request,user_id):
+
+    profile=Profile.get_profile(user_id)
+    print(profile)
+    return render(request,'profile.html',{'profile':profile})
+
+    
